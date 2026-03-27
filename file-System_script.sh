a07184cc48bcc25402de999a75527880dbc4ed8b
josev2609 <jovelascoso@uide.edu.ec>
2026-03-27 15:47:04 +0000

    xz

--- file-System_script.sh
+++ file-System_script.sh
@@ -11,7 +11,10 @@ cat cpuinfo
 cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/
 #change directory 
 sudo mv saludo.c /bin 
-#move that archive to bin 
+#move that files to bin 
  ls | grep "saludo"
- #found the archive 
- 
+ #found the files 
+ pwd
+ #Show the directory that we are working 
+ ls -F
+ #Shows the types of files
