3a5df904762965aa0335a5de88ffa096056a3eb8
josev2609 <jovelascoso@uide.edu.ec>
2026-03-27 15:44:15 +0000

    cz

--- file-System_script.sh
+++ file-System_script.sh
@@ -8,3 +8,10 @@ ls
 #list proc directory 
 cat cpuinfo 
 #shows cpu information 
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/
+#change directory 
+sudo mv saludo.c /bin 
+#move that archive to bin 
+ ls | grep "saludo"
+ #found the archive 
+ 
