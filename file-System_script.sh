1bc973b9fb94d83d80e63b7df7440f919eb8064b
josev2609 <jovelascoso@uide.edu.ec>
2026-03-27 14:45:24 +0000

    as

--- file-System_script.sh
+++ file-System_script.sh
@@ -2,3 +2,9 @@ cd /dev
 #change directory to dev directory 
 ls
 #list archives and directory 
+cd /proc 
+#change directory to proc directory 
+ls 
+#list proc directory 
+cat cpuinfo 
+#shows cpu information 
