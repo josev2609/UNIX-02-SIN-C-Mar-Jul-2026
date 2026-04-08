a096aa0c80200d5c59db06d63204b509a4e29f71
josev2609 <jovelascoso@uide.edu.ec>
2026-04-08 21:36:54 +0000

    wip:stat

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,12 @@ ls -lai
 #l=list a=all i=inode
 ls -l -a -i
 #the same options but in a separate way 
+stat .
+ File: .
+  Size: 4096            Blocks: 8          IO Block: 4096   directory
+Device: 7,6     Inode: 1573068     Links: 2
+Access: (0777/drwxrwxrwx)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+Access: 2026-04-08 21:08:24.379729742 +0000
+Modify: 2026-04-08 21:08:23.158729720 +0000
+Change: 2026-04-08 21:08:23.158729720 +0000
+ Birth: 2026-04-08 21:08:23.158729720 +0000
